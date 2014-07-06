9732ce38d39232c771d547ef734520608014a7d4
Torstein Nicolaysen <tnicolaysen@gmail.com>
2014-07-06 19:55:51 +0200

    Removed old client

--- ConfigRemedy.Client/Scripts/directives.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('app.directives', [])
-
-    .directive('appVersion', ['version', (version : string) => {
-        return (scope, elm, attrs) => {
-            elm.text(version);
-        };
-    }]);
--- ConfigRemedy.Client/Scripts/filters.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('app.filters', [])
-
-    .filter('interpolate', ['version', (version) => {
-        return (text) => {
-            return String(text).replace(/\%VERSION\%/mg, version);
-        }
-    }]);
