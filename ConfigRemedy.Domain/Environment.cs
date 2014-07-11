dbde04f0d30c99915c6813f157d25e491e59d0d3
Torstein Nicolaysen <tnicolaysen@gmail.com>
2014-07-11 16:27:00 +0200

    Changed ordering temporarily to be based on ShortName

--- ConfigRemedy.Domain/Environment.cs
+++ ConfigRemedy.Domain/Environment.cs
@@ -12,7 +12,7 @@ namespace ConfigRemedy.Domain
         [NotNull]
         public string ShortName { get; set; }
 
-        public short Order { get; set; }
+        //public short Order { get; set; }
         public string LongName { get; set; }
         public string Description { get; set; }
         public string Icon { get; set; }
@@ -20,8 +20,11 @@ namespace ConfigRemedy.Domain
 
         public int CompareTo(Environment other)
         {
+            // TODO: Order needs to be added to allow custom sorting. Use short name for how.
             if (other == null) return 1;
-            return Order.CompareTo(other.Order);
+
+            //return Order.CompareTo(other.Order);
+            return String.Compare(ShortName, other.ShortName, StringComparison.InvariantCultureIgnoreCase);
         }
 
         // TODO: Add validation that id and shortname is present
