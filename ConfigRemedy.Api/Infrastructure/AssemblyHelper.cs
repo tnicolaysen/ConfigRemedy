920d861052263d8886c8d5d928d909d4443a5737
Evgeny Borzenin <evgeny.borzenin@gmail.com>
2014-07-18 17:28:58 +0200

    Added diagnostics module for self diagnostic api.

---
+++ ConfigRemedy.Api/Infrastructure/AssemblyHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace ConfigRemedy.Api.Infrastructure
+{
+    public class AssemblyHelper
+    {
+        public static string GetVersion()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            return  fileVersionInfo.FileVersion;
+        }
+        
+        public static string GetPath()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            return assembly.Location;
+        }
+    }
+}
---
+++ ConfigRemedy.Api/Modules/DiagnosticsModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ConfigRemedy.Api.Annotations;
+using ConfigRemedy.Api.Infrastructure;
+
+namespace ConfigRemedy.Api.Modules
+{
+    [UsedImplicitly]
+    public class DiagnosticsModule : BaseModule
+    {
+        public DiagnosticsModule()
+        {
+            Get["diagnostics/"] = _ =>
+            {
+                return new
+                {
+                    Version = AssemblyHelper.GetVersion(),
+                    Platform = Environment.OSVersion.VersionString,
+                    Is64BitOperatingSystem = Environment.Is64BitOperatingSystem,
+                    PathToExecutable = AssemblyHelper.GetPath(),
+
+                    Links = new
+                    {
+                        ServerLogs = "/diagnostics/serverlogs",
+                        ClientLogs = "/diagnostics/clientlogs",
+                    }
+                };
+            };
+        }
+    }
+}
