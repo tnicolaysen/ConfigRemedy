b75a3c69062efd92c4fb9c00a008bc1d452ebccd
Evgeny Borzenin <evgeny.borzenin@gmail.com>
2014-07-21 12:53:50 +0200

    Fixed port number

--- ConfigRemedy.Api/Constants.cs
+++ ConfigRemedy.Api/Constants.cs
@@ -3,7 +3,7 @@
     public static class Constants
     {
         public const string ApplicationName = "Configuratron";
-        public const int DefaultPort = 2304;
+        public const int DefaultPort = 2403;
         public const string DefaultLogsFolderName = "Logs";
         public const string DefaultDbFolderName = "Data";
         public const string DefaultHostname = "+";
