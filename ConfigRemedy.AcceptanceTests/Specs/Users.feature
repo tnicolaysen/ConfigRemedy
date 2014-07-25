7587d74418fe3ceaa67e8e0015ee6f889e31975c
Evgeny Borzenin <evgeny.borzenin@gmail.com>
2014-07-25 14:20:40 +0200

    Check-in missing files that VS didn't check-in.

---
+++ ConfigRemedy.AcceptanceTests/Specs/Users.feature
@@ -0,0 +1,46 @@
+﻿Feature: Users
+	In order manage available users
+	As a programmer
+	I want to have a REST API to manage them
+
+Background: 
+	Given I have a JSON client
+
+Scenario: Get users when database is empty
+	Given the database is empty
+	When I get available users
+	Then I should get HTTP OK
+	And I should get an empty list
+
+Scenario: Getting a user that don't exist
+	Given an user named "james" exist
+	When I GET an user named "bond"
+	Then I should get HTTP NotFound
+	And I should get an empty body
+
+Scenario: Getting an existing user
+	Given an user named "bar" exist
+	When I GET an user named "bar"
+	Then I should get HTTP 200
+	And I should get an user model with name "bar"
+
+Scenario: Adding user
+	Given the database is empty
+	When I POST a user named "james" 
+	Then I should get HTTP Created
+	And an user named "james" should be persisted
+	And location header should contain url for "users/james"
+	And I should get an user model with name "james"
+	And I should get an the following JSON response: {"id":"users/james","userName":"james","displayName":"james","email":"james","passwordHashed":"_some_hashed_password_"}
+
+Scenario: Adding duplicate user is not allowed
+	Given an user named "foo" exist
+	When I POST a user named "foo" 
+	Then I should get HTTP Forbidden with reason "Duplicates are not allowed"
+	And body should be "Duplicates are not allowed"
+
+Scenario: Getting all users
+	Given an user named "james" exist
+	Given an user named "bond" exist
+	When I get available users
+	Then I should get HTTP 200
---
+++ ConfigRemedy.AcceptanceTests/Steps/UsersSteps.cs
@@ -0,0 +1,85 @@
+﻿using ConfigRemedy.AcceptanceTests.Annotations;
+using ConfigRemedy.Security.Domain;
+using NUnit.Framework;
+using System.Linq;
+using TechTalk.SpecFlow;
+
+namespace ConfigRemedy.AcceptanceTests.Steps
+{
+    [UsedImplicitly, MeansImplicitUse]
+    [Binding]
+    public class UsersSteps : ModuleStepsBase
+    {
+        private const string UsersBaseUrl = "api/users/";
+
+        [When(@"I get available users")]
+        public void WhenIGetAvailableUsers()
+        {
+            Result = Browser.Get(UsersBaseUrl, JsonClient);
+        }
+
+        [When(@"I GET an user named ""(\w+)""")]
+        public void WhenIGetAnUserNamed(string userName)
+        {
+            Result = Browser.Get(UsersBaseUrl + userName, JsonClient);
+        }
+        
+        [Then(@"there should be (\d+) users")]
+        public void ThenThereShouldBeUsers(int numberOfUsers)
+        {
+            using (var session = DbContext.EmbeddedStore.OpenSession())
+            {
+                var result = session.Query<User>().ToList();
+
+                Assert.That(result.Count, Is.EqualTo(numberOfUsers));
+            }
+        }
+
+        [Given(@"an user named ""(.*)"" exist")]
+        public void GivenAnUserNamedExist(string userName)
+        {
+            When(string.Format(@"I POST a user named ""{0}""", userName));
+        }
+
+
+        [When(@"I POST a user named ""(\w+)""")]
+        public void WhenIPostAUserNamed(string userName)
+        {
+            Result = Browser.Post(UsersBaseUrl, with =>
+            {
+                JsonClient(with);
+                with.FormValue("userName", userName);
+                with.FormValue("displayName", userName);
+                with.FormValue("email", userName);
+                with.FormValue("passwordHashed", "_some_hashed_password_");
+            });
+        }
+        [Then(@"an user named ""(\w+)"" should be persisted")]
+        public void ThenAnUserNamedShouldBePersisted(string userName)
+        {
+            using (var session = DbContext.EmbeddedStore.OpenSession())
+            {
+                var result = session
+                    .Query<User>()
+                    .Where(user => user.Username == userName)
+                    .ToList();
+
+                Assert.That(result, Is.Not.Empty, "Did not find the specified user");
+            }
+        }
+
+        [Then(@"I should get an user model with name ""(\w+)""")]
+        public void ThenIShouldGetAnUserModelWithName(string userName)
+        {
+            var user = Deserialize<User>(Result);
+            Assert.That(user, Is.Not.Null);
+            Assert.That(user.Username, Is.EqualTo(userName));
+        }
+        
+        // Setup / boilerplate
+        public UsersSteps(DatabaseContext dbContext)
+            : base(dbContext)
+        {
+        }
+    }
+}
